22b7a23ee01be4c7e8b027cd3a6cb2553c7fb963
Henrique Almeida <joaoh.almeidaaraujo@gmail.com>
2021-10-23 00:44:03 -0300

    Req 09

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -46,13 +46,44 @@ OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enq
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    let output;
+    let expected; 
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    output = getCharacter();
+    expected = undefined;
+    assert.strictEqual(output, expected);
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    output = getCharacter('Arya');
+    expected = {
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.'],
+    };
+    assert.deepStrictEqual(output, expected);
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    output = getCharacter('Brienne');
+    expected = {
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+    };
+    assert.deepStrictEqual(output, expected);
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    output = getCharacter('Melissandre');
+    expected = {
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+    };
+    assert.deepStrictEqual(output, expected);
     // Teste se a função se os parâmetros não são Case Sensitive.
+    output = getCharacter('arya') === getCharacter('ArYa');
+    expected = true;
+    assert.deepStrictEqual(output, expected);
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    output = getCharacter('JH');
+    expected = undefined;
+    assert.deepStrictEqual(output, expected);
   });
 });
